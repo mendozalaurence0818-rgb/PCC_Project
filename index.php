0a0969d55f85628b5f65bff82d0fe59fcebdd54e
mendozalaurence0818-rgb <mendozalaurence0818@gmail.com>
2026-06-06 23:13:59 +0800

    remove forgot password and login with gmail

--- index.php
+++ index.php
@@ -57,11 +57,6 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-8">
-                            <p class="mb-1">
-                                <a href="#">Forgot your password?</a>
-                            </p>
-                        </div>
                         <div class="col-4">
                             <div class="d-grid gap-2">
                                 <button type="submit" class="btn btn-primary">Sign In</button>
@@ -69,15 +64,9 @@
                         </div>
                     </div>
                 </form>
-
-                <div class="social-auth-links text-center mb-3 d-grid gap-2">
-                    <p>- OR -</p>
-                    <a href="#" class="btn btn-danger">
-                        <i class="bi bi-google me-2"></i> Sign in using PCC Email
-                    </a>
-                </div>
             </div>
         </div>
+    </div>
     </div>
 </body>
 
